55b395b11dd5efef5d0157d58a8ca916be94f0ed
JamesFarrant <jfarrant4@gmail.com>
2015-02-02 23:23:32 +0000

    Update hutscraper.py

--- hutscraper.py
+++ hutscraper.py
@@ -1,8 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-f = urlopen('http://www.dotahut.com/heroes/pudge')
+f = urlopen('http://www.dotahut.com/heroes/faceless_void')
 soup = BeautifulSoup(f)
-heroList = soup.findAll('div', attrs={'class': 'name'})
-for i in heroList:
-    print(i)
+heroList = soup.findAll('div', attrs={'class': 'weak-block'})
+# Check the first block of HTML for the counter-hero names
+weak_hero = heroList[0].findAll('div', attrs={'class': 'name'})
+
+for i in weak_hero:
+    print(i.text)
